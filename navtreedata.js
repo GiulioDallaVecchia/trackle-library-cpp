9957bcc8e854c9f7b84eed7c25652c1684206a99
bombardieri <bombardieri.federico@gmail.com>
2022-12-02 14:03:25 +0000

    Deploying to gh-pages from  @ 884a97132143011608247089ce96932f2054286f 🚀

---
+++ navtreedata.js
@@ -0,0 +1,49 @@
+/*
+@licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2019 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Trackle esp-idf", "index.html", [
+    [ "Trackle library", "md__r_e_a_d_m_e.html", [
+      [ "Table of contents", "md__r_e_a_d_m_e.html#autotoc_md1", null ],
+      [ "What is Trackle", "md__r_e_a_d_m_e.html#autotoc_md2", null ],
+      [ "Overview", "md__r_e_a_d_m_e.html#autotoc_md3", [
+        [ "Supported hardware", "md__r_e_a_d_m_e.html#autotoc_md4", null ],
+        [ "Download", "md__r_e_a_d_m_e.html#autotoc_md5", null ],
+        [ "Usage and API", "md__r_e_a_d_m_e.html#autotoc_md6", [
+          [ "Get a Device ID and a private key", "md__r_e_a_d_m_e.html#autotoc_md7", null ],
+          [ "Getting started in C++", "md__r_e_a_d_m_e.html#autotoc_md8", null ],
+          [ "Getting started in C", "md__r_e_a_d_m_e.html#autotoc_md9", null ],
+          [ "Trackle client", "md__r_e_a_d_m_e.html#autotoc_md10", null ]
+        ] ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
